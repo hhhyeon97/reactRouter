4d11ba771f4ac10d4d68482dad875966e0e14cf9
jihyeon <kth083030@naver.com>
2024-03-24 23:32:50 +0900

    link/useNavigate

--- src/page/Aboutpage.js
+++ src/page/Aboutpage.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Aboutpage = () => {
+  const navigate = useNavigate();
+  const goToHomepage = () => {
+    // navigate(내가 가고싶은 곳 주소)
+    navigate('/');
+  };
   return (
     <div>
       <h1>About page!</h1>
+      <button onClick={goToHomepage}>Go to Homepage</button>
     </div>
   );
 };
--- src/page/Homepage.js
+++ src/page/Homepage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
   return (
     <div>
       <h1>Homepage</h1>
+      {/*라우터간에 이동할 수 있게 도와주는 링크*/}
+      <Link to="/about">Go to about page</Link>
     </div>
   );
 };
